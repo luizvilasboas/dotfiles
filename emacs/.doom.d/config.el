003c63b5b048fc188cd8a10ebcf994c93577a0c5
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-10-15 20:42:00 -0300

    feat: add Emacs config

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,10 @@
+;; Doom config
+(setq doom-theme 'doom-1337)
+
+(setq display-line-numbers-type t)
+
+(setq org-directory "~/org/")
+
+;; Drag stuff config
+(drag-stuff-global-mode 1)
+(drag-stuff-define-keys)
---
+++ emacs/.doom.d/packages.el
@@ -0,0 +1,2 @@
+;; Drag stuff package
+(package! drag-stuff)
